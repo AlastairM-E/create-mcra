bc3b970262f5eb38bbc0e6283af44fff084b900d
AlastairM-E <alastairunityemail@gmail.com>
2020-03-18 08:34:48 +0000

    working out hte implation details of imp

--- commands/createBoilerplate.js
+++ commands/createBoilerplate.js
@@ -1,8 +1,24 @@
 const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 function createBoilerplate(arguments) {
-    console.log('create-react-app magic: this will take a couple of minutes so relax.');
-    execSync(`npx create-react-app ${arguments}`);
+
+    const impJson = path.join(__dirname, `../node_modules/mcra-user-preferences/imp.json`);
+    let cli = 'create-react-app';
+    let additionalPackages = null;
+
+    if (fs.existsSync(impJson)){
+        const impJsonComponent = JSON.parse(fs.readSync(impJson));
+        cli = impJsonComponent.cli;
+        additionalPackages = impJsonComponent.packages;
+    };
+
+    
+
+    console.log(`${cli} magic: this will take a couple of minutes so relax.`);
+    execSync(`npx ${cli} ${arguments}`);
+    additionalPackages ? execSync(`yarn install --dev ${additionalPackages}`) : null;
     console.log(`
         The wait is finally over, please:
 
